850ae9b4a7f90d984be57b68392754dc73324fbd
nelaaars <nelaaars@gmail.com>
2020-11-12 21:24:12 +0700

    database

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+   session_start();
+   if(isset($_SESSION['username'])) {
+   header('location:index.php'); }
+   require_once("koneksi.php");
+?>
+
+<title>Login</title>
+<div align='center'>
+  <form action="proseslogin.php" method="post">
+  <h1>Masuk</h1>
+  <table>
+  <tbody>
+  <tr><td>Username</td><td> : <input name="username" type="text"></td></tr>
+  <tr><td>Password</td><td> : <input name="password" type="password"></td></tr>
+  <tr><td colspan="2" align="right"><input value="Login" type="submit"> <input value="Batal" type="reset"></td></tr>
+  </tbody>
+  </table>
+  </form>
+</div>
